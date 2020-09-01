5e7fce4e1cf24ade54d1a089fe17a51aaa7a8327
Pallat Anchaleechamaikorn <pallat.anchaleechamaikorn@ktb.co.th>
2020-09-01 14:20:14 +0700

    allow origin

--- main.go
+++ main.go
@@ -82,6 +82,16 @@ func main() {
 	r := mux.NewRouter()
 	r.Use(klog.Middleware(logger))
 	r.Use(muxtrace.Middleware("my-server"))
+	r.Use(func(handler http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			w.Header().Set("X-XSS-Protection", "1; mode=block")
+			w.Header().Set("X-Frame-Options", "DENY")
+			w.Header().Set("Strict-Transport-Security", "max-age=604800; includeSubDomains; preload")
+			handler.ServeHTTP(w, r)
+		})
+	})
 
 	r.Handle("/metrics", promhttp.Handler())
 	r.Handle("/ok", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
